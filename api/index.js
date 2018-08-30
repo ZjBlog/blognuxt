5c4958d3032bcee3d587276ad368801a6ef41f19
zhangjunTracy <13552960783@163.com>
2018-08-30 21:36:47 +0800

    index

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ router.use((req, res, next) => {
 })
 // Add POST - /api/login
 router.post('/login', (req, res) => {
+  console.info(req.body)
   if (req.body.username === 'demo' && req.body.password === 'demo') {
     req.session.authUser = { username: 'demo' }
     return res.json({ username: 'demo' })
@@ -27,6 +28,7 @@ const users = require('./routes/users')
 
 // Import API Routes
 app.use(users)
+app.use(router)
 
 // Export the server middleware
 module.exports = {
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,62 @@
 <template>
-  <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: index }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
-  </section>
+  <div class="container">
+    <h1>Please login to see the secret content</h1>
+    <form v-if="!$store.state.authUser" @submit.prevent="login">
+      <p v-if="formError" class="error">{{ formError }}</p>
+      <p><i>To login, use <b>demo</b> as username and <b>demo</b> as password.</i></p>
+      <p>Username: <input v-model="formUsername" type="text" name="username" ></p>
+      <p>Password: <input v-model="formPassword" type="password" name="password" ></p>
+      <button type="submit">Login</button>
+    </form>
+    <div v-else>
+      Hello {{ $store.state.authUser.username }}!
+      <pre>I am the secret content, I am shown only when the use is connected.</pre>
+      <p><i>You can also refresh this page, you'll still be connected!</i></p>
+      <button @click="logout">Logout</button>
+    </div>
+    <p><nuxt-link to="/secret">Super secret page</nuxt-link></p>
+  </div>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
-
 export default {
-  async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return { users: data }
-  },
-  head () {
+  data () {
     return {
-      title: 'Users'
+      formError: null,
+      formUsername: '',
+      formPassword: ''
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        await this.$store.dispatch('login', {
+          username: this.formUsername,
+          password: this.formPassword
+        })
+        this.formUsername = ''
+        this.formPassword = ''
+        this.formError = null
+      } catch (e) {
+        this.formError = e.message
+      }
+    },
+    async logout () {
+      try {
+        await this.$store.dispatch('logout')
+      } catch (e) {
+        this.formError = e.message
+      }
     }
   }
 }
 </script>
 
-<style scoped>
-.title
-{
-  margin: 30px 0;
+<style>
+.container {
+  padding: 100px;
 }
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
+.error {
+  color: red;
 }
 </style>
